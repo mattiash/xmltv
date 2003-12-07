769f2b3ed37ff94ac6e7c0198987e49e6f14b9e6
epaepa <epaepa>
2003-12-07 22:28:55 +0000

    Version 0.5.25.

--- grab/na/ZapListings.pm
+++ grab/na/ZapListings.pm
@@ -1,4 +1,4 @@
-# $Id: ZapListings.pm,v 1.75 2003/11/30 18:40:33 epaepa Exp $
+# $Id: ZapListings.pm,v 1.76 2003/12/07 22:28:56 epaepa Exp $
 
 #
 # Special thanks to Stephen Bain for helping me play catch-up with
@@ -152,7 +152,7 @@ sub new
 				  env_proxy => 1,
 				  timeout => 180);
     bless ($self, $class);
-    $self->agent('xmltv/0.5.24');
+    $self->agent('xmltv/0.5.25');
     return $self;
 }
 
