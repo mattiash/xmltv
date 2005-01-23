19e22a9159f17924b0db38b2b211f7ca68e402c7
epaepa <epaepa>
2005-01-23 22:38:08 +0000

    Version 0.5.38.  Noted reduced dependencies for tv_grab_no.

--- lib/exe_wrap.pl
+++ lib/exe_wrap.pl
@@ -1,6 +1,6 @@
 #!perl -w
 #
-# $Id: exe_wrap.pl,v 1.54 2004/12/13 19:26:58 mnbjhguyt Exp $
+# $Id: exe_wrap.pl,v 1.55 2005/01/23 22:38:09 epaepa Exp $
 # This is a quick XMLTV shell routing to use with the windows exe
 #
 # A single EXE is needed to allow sharing of modules and dlls of all the
@@ -69,7 +69,7 @@ print STDERR "Timezone is $ENV{TZ}\n" unless $opt_quiet;
 $cmd = shift || "";
 
 # --version (and abbreviations thereof)
-my $VERSION = '0.5.37';
+my $VERSION = '0.5.38';
 if (index('--version', $cmd) == 0 and length $cmd >= 3) {
     print "xmltv $VERSION\n";
     exit;
