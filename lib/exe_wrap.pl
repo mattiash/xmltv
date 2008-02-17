28eb66e05374771f6ba8757b11ec13c0e85c6ad4
rmeden <rmeden>
2008-02-17 07:08:59 +0000

    prepare for 0.5.51 release

--- lib/exe_wrap.pl
+++ lib/exe_wrap.pl
@@ -1,6 +1,6 @@
 #!perl -w
 #
-# $Id: exe_wrap.pl,v 1.77 2007/11/05 08:14:27 rmeden Exp $
+# $Id: exe_wrap.pl,v 1.78 2008/02/17 07:09:00 rmeden Exp $
 # This is a quick XMLTV shell routing to use with the windows exe
 #
 # A single EXE is needed to allow sharing of modules and dlls of all the
@@ -69,7 +69,7 @@ print STDERR "Timezone is $ENV{TZ}\n" unless $opt_quiet;
 $cmd = shift || "";
 
 # --version (and abbreviations thereof)
-my $VERSION = '0.5.50';
+my $VERSION = '0.5.51';
 if (index('--version', $cmd) == 0 and length $cmd >= 3) {
     print "xmltv $VERSION\n";
     exit;
